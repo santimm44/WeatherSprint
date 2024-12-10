7a352f4fde71010a1b1529bd70b887bba436228c
MundoDeSanti <santiagomm44@gmail.com>
2024-12-09 23:07:01 -0800

    Played around with the API for current weather. API call is working and I am able to display the name, low temp, high temp. Need to start grabbing the weather description, main, and icon. Afterwards I should begin setting up Geolocation after I pull the data and place them where they belong. so much to do, so little time to do them

--- script/app.js
+++ script/app.js
@@ -1,17 +1,28 @@
-//current weather
-//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-
-//5 day forecast
-//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-
 import { APIKEY } from "./hidden.js";
 
+let lon = -121.2908;
+let lat = 37.9577;
+
+const currentDate = new Date();
+
+let lowTempToday = document.getElementById("lowTempToday");
+let highTempToday = document.getElementById("highTempToday");
+let cityName = document.getElementById("cityName");
+let todaysDate = document.getElementById("todaysDate");
+
+
 async function todaysWeather(){
-    let apiData = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=-121.2908&appid=${APIKEY}`);
+    let apiData = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}`);
     let returnedData = await apiData.json();
 
+    cityName.innerText = returnedData.name;
+    todaysDate.innerText = currentDate;
+    lowTempToday.innerText = returnedData.main.temp_min;
+    highTempToday.innerText = returnedData.main.temp_max;
+
+
     //console.log(returnedData)
-    return returnedData;
+    //return returnedData;
 }
 //todaysWeather();
 
