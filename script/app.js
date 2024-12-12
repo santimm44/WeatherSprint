93d63f06c1fedd1dcdc70ad672d8e96901d0a3a5
MundoDeSanti <santiagomm44@gmail.com>
2024-12-11 16:24:39 -0800

    Been reading API docs mainly today. Trying to figure out how tto get the daily high/low. I think I am close, but will try finishiit later tonight

--- script/app.js
+++ script/app.js
@@ -5,34 +5,70 @@ let lat = 37.9577;
 
 const currentDate = new Date();
 
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+//list of text that will be altered
 let lowTempToday = document.getElementById("lowTempToday");
 let highTempToday = document.getElementById("highTempToday");
 let cityName = document.getElementById("cityName");
 let todaysDate = document.getElementById("todaysDate");
+let weatherChance = document.getElementById("weatherChance")
+let dayOfWeek = document.getElementById("dayOfWeek");
+let cardOneDay = document.getElementById("cardOneDay");
+let cardTwoDay = document.getElementById("cardTwoDay");
+let cardThreeDay = document.getElementById("cardThreeDay");
+let cardFourDay = document.getElementById("cardFourDay");
+let cardFiveDay = document.getElementById("cardFiveDay");
+let cardOneIcon = document.getElementById("cardOneIcon");
+let cardTwoIcon = document.getElementById("cardTwoIcon");
+let cardThreeIcon = document.getElementById("cardThreeIcon");
+let cardFourIcon = document.getElementById("cardFourIcon");
+let cardFiveIcon = document.getElementById("cardFiveIcon");
 
 
-async function todaysWeather(){
-    let apiData = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}`);
+
+async function weatherForecast(){
+    // let apiData = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=imperial`);
+    // let returnedData = await apiData.json();
+
+    //cityName.innerText = returnedData.name;
+    todaysDate.innerText = `${currentDate.toLocaleString('default', { month: 'short' })} ${currentDate.getDate()}th, ${currentDate.getFullYear()}`;
+    dayOfWeek.innerText = `${days[currentDate.getDay()]}`;
+    //weatherChance.innerText = returnedData.weather[0].description; 
+    //lowTempToday.innerText = returnedData.main.temp_min; // not accurate. needs to be changed to the actual daily low
+    //highTempToday.innerText = returnedData.main.temp_max; // not accurate. needs to be changed to the actual daily high
+
+    cardOneDay.innerText = `${days[dayOfTheWeek(1)]}`;
+    cardTwoDay.innerText = `${days[dayOfTheWeek(2)]}`;
+    cardThreeDay.innerText = `${days[dayOfTheWeek(3)]}`;
+    cardFourDay.innerText = `${days[dayOfTheWeek(4)]}`;
+    cardFiveDay.innerText =`${days[dayOfTheWeek(5)]}`;
+    
+    //cardOneIcon.src = `https://openweathermap.org/img/wn/${returnFiveDayData}@2x.png`
+}
+//weatherForecast();
+
+function dayOfTheWeek(addedDay){
+    let day = currentDate.getDay(); // returns a number between 0(sunday)-6(Saturday)
+    
+    let returnDay = addedDay + day;
+    if (returnDay > 6){
+        returnDay -= 7;
+    }
+
+    return returnDay
+}
+
+async function fiveDayWeatherInformation(){
+
+    let apiData = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKEY}`);
     let returnedData = await apiData.json();
 
-    cityName.innerText = returnedData.name;
-    todaysDate.innerText = currentDate;
-    lowTempToday.innerText = returnedData.main.temp_min;
-    highTempToday.innerText = returnedData.main.temp_max;
 
-
-    //console.log(returnedData)
-    //return returnedData;
+    for (let i = 0; i <5; i++){
+        for(let j=0; j<8; j++){
+            cardOneIcon.src = `https://openweathermap.org/img/wn/@2x.png`;
+        }
+    }
 }
-//todaysWeather();
-
-
-async function fiveDayForecast(){
-    let apiData = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=37.9577&lon=-121.2908&appid=${APIKEY}`);
-    let returnedData = await apiData.json();
-
-    //console.log(returnedData);
-    return returnedData;
-}
-
-//fiveDayForecast();
