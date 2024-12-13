3faedaf698143fc75346e8c85b41300143cb5468
MundoDeSanti <santiagomm44@gmail.com>
2024-12-13 02:17:09 -0800

    created some more functionality for local storage. It is able to add things in. Working on removing items. For the drop down menu I am able to display the list of cities. I am working on an if-statement to hide the list if clicked outside the dropdown and creating a click function that would load the page with with different information

--- script/app.js
+++ script/app.js
@@ -42,6 +42,7 @@ let cardFourDescription = document.getElementById("cardFourDescription");
 let cardFiveDescription = document.getElementById("cardFiveDescription");
 
 let inputField = document.getElementById("inputField");
+let searchbar = document.getElementById("searchbar");
 
 async function weatherForecast(searchCityName) {
     let apiData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCityName}&appid=${APIKEY}&units=imperial`);
@@ -160,9 +161,7 @@ inputField.addEventListener("click", function () {
     //i want to create a dropdown when user clicks the search bar
     //drop down will populate the favorites of the user
 
-
-
-
+    createElements();
 })
 
 favoriteDiv.addEventListener("click", function () {
@@ -173,13 +172,65 @@ favoriteDiv.addEventListener("click", function () {
     if (currentSVG.classList.contains("star-regular")) {
         favoriteDiv.innerHTML = `<svg class="star star-favorite" alt="star icon - favorite" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>`;
         //beneath I will need to add the city to the list of favorites in the local storage
+        //saveToLocalStorage() //pass in city name to create key
+        saveToLocalStorage(cityName.innerText);
     } else {
         favoriteDiv.innerHTML = `<svg class="star star-regular" alt="star icon - regular" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.7 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z"/></svg>`;
         //beneath I will need to remove the city from the list of favorites in the local storage.
+        //removeFromLocalStorage() //pass in city name to remove key
+        //removeFromLocalStorage(cityName.innerText);
     }
 })
 
+favoriteDiv.addEventListener("change", function(){
+    let cityArray = getFromLocalStorage();
 
-//fiveDayWeatherInformation()
-//weatherForecast();
+    if(cityArray.includes(cityName.innerText)){
+        favoriteDiv.innerHTML = `<svg class="star star-regular" alt="star icon - regular" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.7 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z"/></svg>`;
+    }
+    else{
+        favoriteDiv.innerHTML = `<svg class="star star-favorite" alt="star icon - favorite" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>`;
+    }
+})
 
+function saveToLocalStorage(storeCityName){
+    let cityArray = getFromLocalStorage();
+
+    if(!cityArray.includes(storeCityName)){
+        cityArray.push(storeCityName);
+    }
+
+    localStorage.setItem("storedCityNames", JSON.stringify(cityArray));
+
+}
+
+function removeFromLocalStorage(removeCityName){
+    let cityArray = getFromLocalStorage();
+
+    localStorage.removeItem("storedCityNames", removeCityName);
+}
+
+function getFromLocalStorage(){
+    let localStorageData = localStorage.getItem("storedCityNames");
+
+    if (localStorageData == null){
+        return[]
+    }
+    return JSON.parse(localStorageData);
+}
+
+function createElements(){
+    let favoriteList = getFromLocalStorage();
+
+    favoriteList.map(cities => {
+        console.log(cities)
+        let p = document.createElement("p");
+        let div = document.createElement("div");
+        div.className = "drop-down__create-div";
+        p.innerText = cities;
+
+
+        searchbar.appendChild(div);
+        div.appendChild(p);
+    })
+}
